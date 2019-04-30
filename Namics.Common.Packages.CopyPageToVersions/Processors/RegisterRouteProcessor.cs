7a80c210a3d001114d1f931e685dd02ffd377349
Michael Guster <michael.guster@namics.com>
2019-04-30 14:15:59 +0200

    sc90 helix update - update route registration for copy page to versions dialog, that it doesn't interfere with the experience profile route registration

--- Namics.Common.Packages.CopyPageToVersions/Processors/RegisterRouteProcessor.cs
+++ Namics.Common.Packages.CopyPageToVersions/Processors/RegisterRouteProcessor.cs
@@ -1,5 +1,6 @@
-﻿using Sitecore.Pipelines;
-using System.Web.Http;
+﻿using System.Web.Http;
+using System.Web.Routing;
+using Sitecore.Pipelines;
 
 namespace Namics.Common.Packages.CopyPageToVersions.Processors
 {
@@ -12,14 +13,20 @@ namespace Namics.Common.Packages.CopyPageToVersions.Processors
         /// Processes the pipeline, which registeres the route for the copy page to versions api controller:
         /// /api/copypagetoversions/copypagetoversion
         /// </summary>
-        /// <param name="args"></param>
+        /// <param name="args">The initialize pipeline arguments</param>
         public void Process(PipelineArgs args)
         {
-            HttpConfiguration config = GlobalConfiguration.Configuration;
+            GlobalConfiguration.Configure(RegisterRoutes);
+        }
 
-            config.Routes.MapHttpRoute("CopyPageToVerions",
+        private static void RegisterRoutes(HttpConfiguration config)
+        {
+            // Registeres the copy page to versions dialog action
+            RouteTable.Routes.MapHttpRoute(
+                "CopyPageToVerions",
                 "sitecore/shell/api/{controller}/{action}/{id}",
-                new {controller = "copypagetoversions", action = "copypagetoversion", id = RouteParameter.Optional});
+                new { controller = "copypagetoversions", action = "copypagetoversion", id = RouteParameter.Optional }
+            );
         }
     }
 }
